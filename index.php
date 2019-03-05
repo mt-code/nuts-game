55435be5ca27873be56f93b6addd2a369ba7e235
mc-soft <matthew.croston@btinternet.com>
2019-03-05 10:10:15 +0000

    Added reset CSS and required fonts

--- index.php
+++ index.php
@@ -7,10 +7,13 @@
         <meta name="description" content="Can you find the botanical but?" />
         <meta name="viewport" content="device-width=1.0, initial-scale=1.0" />
 
+        <link rel="stylesheet" href="https://use.typekit.net/jrf5itg.css">
         <link rel="stylesheet" href="assets/css/main.css">
     </head>
 
-    <body>
+    <body class="home">
+        <h1>Find the nut*</h1>
+
         <div class="container">
 
         </div>
