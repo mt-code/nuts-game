ec68c3534fe1e22e239383b08c0943b8a3a0eccd
mc-soft <matthew.croston@btinternet.com>
2019-03-05 10:04:18 +0000

    Added CSS/JS files

--- index.php
+++ index.php
@@ -6,11 +6,15 @@
         <meta charset="UTF-8" />
         <meta name="description" content="Can you find the botanical but?" />
         <meta name="viewport" content="device-width=1.0, initial-scale=1.0" />
+
+        <link rel="stylesheet" href="assets/css/main.css">
     </head>
 
     <body>
         <div class="container">
-            
+
         </div>
+
+        <script type="text/javascript" src="assets/js/main.js"></script>
     </body>
 </html>
