9595e4337b517d29a84ae7da92d36af1ddd700d9
mc-soft <matthew.croston@btinternet.com>
2019-03-06 12:36:27 +0000

    Added nuts to the homepage

--- index.php
+++ index.php
@@ -12,11 +12,77 @@
     </head>
 
     <body class="home">
-        <h1>Find the nut*</h1>
 
-        <div class="container">
 
-        </div>
+        <section class="content">
+            <h1>Find the nut*</h1>
+
+            <div class="nuts">
+
+                <!-- FIRST ROW -->
+                <div class="nut">
+                    <img src="assets/img/nuts/almond.jpg" alt="Almond">
+                    <p class="name">Almond</p>
+                </div>
+
+                <div class="nut">
+                    <img src="assets/img/nuts/brazil.jpg" alt="Brazil">
+                    <p class="name">Brazil</p>
+                </div>
+
+                <div class="nut">
+                    <img src="assets/img/nuts/cashew.jpg" alt="Cashew">
+                    <p class="name">Cashew</p>
+                </div>
+
+                <div class="nut">
+                    <img src="assets/img/nuts/coconut.jpg" alt="Coconut">
+                    <p class="name">Coconut</p>
+                </div>
+
+                <!-- SECOND ROW -->
+                <div class="nut">
+                    <img src="assets/img/nuts/hazelnut.jpg" alt="Hazelnut">
+                    <p class="name">Hazel</p>
+                </div>
+
+                <div class="nut">
+                    <img src="assets/img/nuts/macadamia.jpg" alt="Macadamia">
+                    <p class="name">Macadamia</p>
+                </div>
+
+                <div class="nut">
+                    <img src="assets/img/nuts/metalnuts.jpg" alt="Metal Nuts">
+                    <p class="name">Nuts</p>
+                </div>
+
+                <div class="nut">
+                    <img src="assets/img/nuts/peanut.jpg" alt="Peanut">
+                    <p class="name">Peanut</p>
+                </div>
+
+                <!-- THIRD ROW -->
+                <div class="nut">
+                    <img src="assets/img/nuts/pecan.jpg" alt="Pecan">
+                    <p class="name">Pecan</p>
+                </div>
+
+                <div class="nut">
+                    <img src="assets/img/nuts/pinenut.jpg" alt="Pinenut">
+                    <p class="name">Pinenut</p>
+                </div>
+
+                <div class="nut">
+                    <img src="assets/img/nuts/pistachio.jpg" alt="Pistachio">
+                    <p class="name">Pistachio</p>
+                </div>
+
+                <div class="nut">
+                    <img src="assets/img/nuts/walnut.jpg" alt="Walnut">
+                    <p class="name">Walnut</p>
+                </div>
+            </div>
+        </section>
 
         <script type="text/javascript" src="assets/js/main.js"></script>
     </body>
